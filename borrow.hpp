01f9707f55430c9a3b87bf06c631a26c9fd26dcb
ZisIsNotZis <zisisnotzis@gmail.com>
2017-01-28 17:50:58 -0500

    move/copy constrcutor

--- borrow.hpp
+++ borrow.hpp
@@ -5,19 +5,28 @@
 template<class A,bool own=true> struct box{
 	box(){}
 	box(const A& b):a(new A(b)){}
-	box(A&& b):a(new A(std::move(b))){}
+	box(A& b):a(new A(std::move(b))){}
+	box(A&& b):box(b){}
+	box(A *b):a(b){}
+	box(const box<A>& b):a(new A(b.a)){}
+	box(box<A>& b):a(b.a){b.a=0;}
+	box(box<A>&& b):box(b.a){}
 	~box(){delete a;}
-	box<A,false> borrow()const{return box<A,false>(*a);}
-	box<A,true> move(){box<A,true> b;b.a=a;a=0;return b;}
-	box<A,true> copy()const{return box<A,true>(std::cref(*a));}
+	box<A,false> borrow() const{return box<A,false>(*a);}
+	box<A> move(){return box<A>(*this);}
+	box<A> copy() const{return box<A>(std::cref(*a));}
 	A& operator*(){return *a;}
 	A *a;
 };
 template<class A> struct box<A,false>{
 	box(){}
 	box(A& b):a(&b){}
+	box(A&& b):box(b){}
+	box(A *b):a(b){}
+	template<bool B> box(box<A,B>& b):a(b.a){}
+	template<bool B> box(box<A,B>&& b):box(b){}
 	box<A,false> borrow()const{return box<A,false>(*a);}
-	box<A,true> copy()const{return box<A,true>(std::cref(*a));}
+	box<A> copy()const{return box<A>(std::cref(*a));}
 	A& operator*(){return *a;}
 	A *a;
 };
